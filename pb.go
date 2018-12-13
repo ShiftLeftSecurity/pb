1a3d00a470aab42b8e613f7cc337dcb18f770549
codylaeder <cody@shiftleft.io>
2018-12-12 16:46:14 -0800

    hopefully that is good to go

--- pb.go
+++ pb.go
@@ -411,11 +411,11 @@ func (pb *ProgressBar) write(total, current int64) {
 	case isFinish:
 		return
 	case pb.Output != nil:
-		fmt.Fprint(pb.Output, "\r"+out+end)
+		fmt.Fprint(pb.Output, ClearLinePrefixString+out+end)
 	case pb.Callback != nil:
 		pb.Callback(out + end)
 	case !pb.NotPrint:
-		fmt.Print("\r" + out + end)
+		fmt.Print(ClearLinePrefixString + out + end)
 	}
 }
 
